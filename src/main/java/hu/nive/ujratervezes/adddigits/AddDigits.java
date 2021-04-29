c78a95102ce0fca5feb2b4f6c84bdf999098ce94
Sipkó Miklós <miklossipko@gmail.com>
2021-04-29 10:14:41 +0200

    All tests passed

--- src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
+++ src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
@@ -1,5 +1,20 @@
 package hu.nive.ujratervezes.adddigits;
 
 public class AddDigits {
-    //todo: implement your solution here
+
+    public int addDigits(String input) {
+        if (input == null || input.equals("")) {
+            return -1;
+        }
+        int result = 0;
+        for (int i = 0; i < input.length(); i++) {
+            try {
+                result += Integer.parseInt(String.valueOf(input.charAt(i)));
+            } catch (NumberFormatException e) {
+                continue;
+            }
+
+        }
+        return result;
+    }
 }
